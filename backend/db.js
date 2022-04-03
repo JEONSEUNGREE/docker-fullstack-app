f7cd6a9074fedb85613a7a87d30ccb14735d2c0f
root <jws08219@naver.com>
2022-04-03 22:36:53 +0900

    travis aws

--- backend/db.js
+++ backend/db.js
@@ -2,10 +2,10 @@ const mysql = require("mysql")
 // AWS RDS를 위한 작업
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: "mysql",
-    user: 'root',
-    password: 'seungree',
-    database: 'myapp'
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: process.env.MYSQL_DATABSE
 })
 
 exports.pool = pool;
